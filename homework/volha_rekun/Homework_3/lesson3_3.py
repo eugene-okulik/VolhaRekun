b77bc6a82bed15570f32cb83c3ebf7020678726b
RekunVolha <volha.rekun9@gmail.com>
2025-02-20 22:14:42 +0100

    lesson_3 update_2_0

--- homework/volha_rekun/Homework_3/lesson3_3.py
+++ homework/volha_rekun/Homework_3/lesson3_3.py
@@ -1,10 +1,10 @@
 # Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
-from math import sqrt 
+from math import sqrt
 
 
 chislo_1 = 34
 chislo_2 = 49
 srednee_arifm = (chislo_1 + chislo_2) / 2
-srednee_geometr = sqrt (chislo_1 * chislo_2)
+srednee_geometr = sqrt(chislo_1 * chislo_2)
 print("srednee_arifm =", srednee_arifm)
 print("srednee_geometr =", srednee_geometr)
