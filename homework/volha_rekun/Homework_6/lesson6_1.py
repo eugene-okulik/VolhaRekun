d14245d760a7b72b07208ab2852d8abcd1b8c8ca
RekunVolha <volha.rekun9@gmail.com>
2025-03-05 20:53:26 +0100

    lesson 6 hw updated

--- homework/volha_rekun/Homework_6/lesson6_1.py
+++ homework/volha_rekun/Homework_6/lesson6_1.py
@@ -1,13 +1,13 @@
 # Напишите программу, которая добавляет ‘ing’ в конец слов (к каждому слову) в тексте
 # “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,
 # dignissim vitae libero” и после этого выводит получившийся текст на экран.
-# Знаки препинания не должны оказаться внутри слова. Если после слова идет запятая или точка, этот знак препинания должен
+# Знаки препинания не должны оказаться внутри слова. Если после слова идет запятая или точка, знак препинания должен
 # идти после того же слова, но уже преобразованного.
 
-text = '''Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, 
+text = '''Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl,
 facilisis vitae semper at, dignissim vitae libero'''
 
-words = text.split()
+words = text.replace("\n", " ").split()
 words_updated = []
 
 for word in words:
